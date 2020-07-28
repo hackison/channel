45ddcfa127091491f824e2fe738b15806dd5ff01
Hackison <tamilhackison@gmail.com>
2020-07-28 18:20:39 +0300

    Add files via upload
    Updated nmap

---
+++ nmap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install git wget build-essential checkinstall libpcre3-dev libssl-dev libpcap-dev
+git clone https://github.com/nmap/nmap.git
+cd nmap/
+exec $SHELL
+./configure
+make && make install
