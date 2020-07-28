b039240999d7d43afc3ac7045a68171e7a28e9c3
Hackison <tamilhackison@gmail.com>
2020-07-28 18:42:38 +0300

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install git wget build-essential checkinstall libpcre3-dev libssl-dev libpcap-dev
+sudo apt-get install -y git wget build-essential checkinstall libpcre3-dev libssl-dev libpcap-dev
 git clone https://github.com/nmap/nmap.git
 cd nmap/
 exec $SHELL
