c2f65b17ce35348438ff7c12453206853ae99b62
Hackison <tamilhackison@gmail.com>
2020-07-28 18:19:08 +0300

    Delete nmap.sh

--- nmap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-sudo apt-get install git wget build-essential checkinstall libpcre3-dev libssl-dev libpcap-dev
-git clone https://github.com/nmap/nmap.git
-cd nmap/
-exec $SHELL
-./configure
-make && make install
